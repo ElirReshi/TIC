75bec4a667a78f3f61e2ac86f7ef440d8bdaca89
ElirReshi <elirreshi@gmail.com>
2016-04-01 08:05:51 +0200

    Formularios con sus llamados

--- ApplicationEF/ApplicationEF/Dashboard.Designer.cs
+++ ApplicationEF/ApplicationEF/Dashboard.Designer.cs
@@ -28,20 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Main = new System.Windows.Forms.MenuStrip();
+            this.dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blacklistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maquinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.responsableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anexosEscolaresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.docenteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignaturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MDI = new System.Windows.Forms.Panel();
+            this.Main.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.Main.BackColor = System.Drawing.Color.White;
+            this.Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dashboardToolStripMenuItem,
+            this.horarioToolStripMenuItem,
+            this.controlToolStripMenuItem,
+            this.blacklistToolStripMenuItem,
+            this.maquinaToolStripMenuItem,
+            this.responsableToolStripMenuItem,
+            this.anexosEscolaresToolStripMenuItem});
+            this.Main.Location = new System.Drawing.Point(20, 60);
+            this.Main.Name = "Main";
+            this.Main.Size = new System.Drawing.Size(760, 24);
+            this.Main.TabIndex = 0;
+            this.Main.Text = "Main";
+            // 
+            // dashboardToolStripMenuItem
+            // 
+            this.dashboardToolStripMenuItem.Name = "dashboardToolStripMenuItem";
+            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.dashboardToolStripMenuItem.Text = "Dashboard";
+            this.dashboardToolStripMenuItem.Click += new System.EventHandler(this.dashboardToolStripMenuItem_Click);
+            // 
+            // horarioToolStripMenuItem
+            // 
+            this.horarioToolStripMenuItem.Name = "horarioToolStripMenuItem";
+            this.horarioToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.horarioToolStripMenuItem.Text = "Horario";
+            this.horarioToolStripMenuItem.Click += new System.EventHandler(this.horarioToolStripMenuItem_Click);
+            // 
+            // controlToolStripMenuItem
+            // 
+            this.controlToolStripMenuItem.Name = "controlToolStripMenuItem";
+            this.controlToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.controlToolStripMenuItem.Text = "Control";
+            this.controlToolStripMenuItem.Click += new System.EventHandler(this.controlToolStripMenuItem_Click);
+            // 
+            // blacklistToolStripMenuItem
+            // 
+            this.blacklistToolStripMenuItem.Name = "blacklistToolStripMenuItem";
+            this.blacklistToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.blacklistToolStripMenuItem.Text = "Blacklist";
+            this.blacklistToolStripMenuItem.Click += new System.EventHandler(this.blacklistToolStripMenuItem_Click);
+            // 
+            // maquinaToolStripMenuItem
+            // 
+            this.maquinaToolStripMenuItem.Name = "maquinaToolStripMenuItem";
+            this.maquinaToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.maquinaToolStripMenuItem.Text = "Maquina";
+            this.maquinaToolStripMenuItem.Click += new System.EventHandler(this.maquinaToolStripMenuItem_Click);
+            // 
+            // responsableToolStripMenuItem
+            // 
+            this.responsableToolStripMenuItem.Name = "responsableToolStripMenuItem";
+            this.responsableToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.responsableToolStripMenuItem.Text = "Responsable";
+            this.responsableToolStripMenuItem.Click += new System.EventHandler(this.responsableToolStripMenuItem_Click);
+            // 
+            // anexosEscolaresToolStripMenuItem
+            // 
+            this.anexosEscolaresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.docenteToolStripMenuItem,
+            this.asignaturaToolStripMenuItem,
+            this.grupoToolStripMenuItem,
+            this.turnoToolStripMenuItem});
+            this.anexosEscolaresToolStripMenuItem.Name = "anexosEscolaresToolStripMenuItem";
+            this.anexosEscolaresToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.anexosEscolaresToolStripMenuItem.Text = "Anexos Escolares";
+            // 
+            // docenteToolStripMenuItem
+            // 
+            this.docenteToolStripMenuItem.Name = "docenteToolStripMenuItem";
+            this.docenteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.docenteToolStripMenuItem.Text = "Docente";
+            this.docenteToolStripMenuItem.Click += new System.EventHandler(this.docenteToolStripMenuItem_Click);
+            // 
+            // asignaturaToolStripMenuItem
+            // 
+            this.asignaturaToolStripMenuItem.Name = "asignaturaToolStripMenuItem";
+            this.asignaturaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.asignaturaToolStripMenuItem.Text = "Asignatura";
+            this.asignaturaToolStripMenuItem.Click += new System.EventHandler(this.asignaturaToolStripMenuItem_Click);
+            // 
+            // grupoToolStripMenuItem
+            // 
+            this.grupoToolStripMenuItem.Name = "grupoToolStripMenuItem";
+            this.grupoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.grupoToolStripMenuItem.Text = "Grupo";
+            this.grupoToolStripMenuItem.Click += new System.EventHandler(this.grupoToolStripMenuItem_Click);
+            // 
+            // turnoToolStripMenuItem
+            // 
+            this.turnoToolStripMenuItem.Name = "turnoToolStripMenuItem";
+            this.turnoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.turnoToolStripMenuItem.Text = "Turno";
+            this.turnoToolStripMenuItem.Click += new System.EventHandler(this.turnoToolStripMenuItem_Click);
+            // 
+            // MDI
+            // 
+            this.MDI.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MDI.Location = new System.Drawing.Point(20, 84);
+            this.MDI.Name = "MDI";
+            this.MDI.Size = new System.Drawing.Size(760, 496);
+            this.MDI.TabIndex = 1;
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.MDI);
+            this.Controls.Add(this.Main);
+            this.MainMenuStrip = this.Main;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
+            this.Main.ResumeLayout(false);
+            this.Main.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip Main;
+        private System.Windows.Forms.Panel MDI;
+        private System.Windows.Forms.ToolStripMenuItem dashboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blacklistToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maquinaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem responsableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem anexosEscolaresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem docenteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignaturaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grupoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turnoToolStripMenuItem;
     }
 }
 
--- ApplicationEF/ApplicationEF/Dashboard.cs
+++ ApplicationEF/ApplicationEF/Dashboard.cs
@@ -7,14 +7,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
+using ApplicationEF.Formularios;
 
 namespace ApplicationEF
 {
-    public partial class Dashboard : Form
+    public partial class Dashboard : MetroForm
     {
         public Dashboard()
         {
             InitializeComponent();
+        }
+
+        private void FormPanel(Form Hijo)
+        {
+            if (this.MDI.Controls.Count > 0)
+                this.MDI.Controls.RemoveAt(0);
+            Hijo.TopLevel = false;
+            Hijo.FormBorderStyle = FormBorderStyle.None;
+            Hijo.Dock = DockStyle.Fill;
+            this.MDI.Controls.Add(Hijo);
+            this.MDI.Tag = Hijo;
+            Hijo.Show();
+        }
+
+        private void turnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FTurno>().FirstOrDefault();
+            FTurno hijo = form ?? new FTurno();
+            FormPanel(hijo);
+        }
+
+        private void grupoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FGrupo>().FirstOrDefault();
+            FGrupo hijo = form ?? new FGrupo();
+            FormPanel(hijo);
+        }
+
+        private void asignaturaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FAsignatura>().FirstOrDefault();
+            FAsignatura hijo = form ?? new FAsignatura();
+            FormPanel(hijo);
+        }
+
+        private void docenteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FDocente>().FirstOrDefault();
+            FDocente hijo = form ?? new FDocente();
+            FormPanel(hijo);
+        }
+
+        private void responsableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FResponsable>().FirstOrDefault();
+            FResponsable hijo = form ?? new FResponsable();
+            FormPanel(hijo);
+        }
+
+        private void maquinaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FMaquina>().FirstOrDefault();
+            FMaquina hijo = form ?? new FMaquina();
+            FormPanel(hijo);
+        }
+
+        private void blacklistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FBlacklist>().FirstOrDefault();
+            FBlacklist hijo = form ?? new FBlacklist();
+            FormPanel(hijo);
+        }
+
+        private void controlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FControl>().FirstOrDefault();
+            FControl hijo = form ?? new FControl();
+            FormPanel(hijo);
+        }
+
+        private void horarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FHorario>().FirstOrDefault();
+            FHorario hijo = form ?? new FHorario();
+            FormPanel(hijo);
+        }
+
+        private void dashboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Application.OpenForms.OfType<FDashboard>().FirstOrDefault();
+            FDashboard hijo = form ?? new FDashboard();
+            FormPanel(hijo);
         }
     }
 }
